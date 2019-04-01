184955975dc3ca60e0b3dd627aaf571d1303f1bb
CalieR <calielrushton@gmail.com>
2019-04-01 14:37:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-animal = ""
+const animal = "cat";
 
 function myAnimal() {
-  return animal
+  // engine will not look up the chain as animal is declared in this scope
+  const animal = "dog";
+  return animal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animal;
 }
 
 function add2(n) {
-  return n + two
+  let two = 2;
+  return n + two;
 
   // Feel free to move things around!
-  two = 2
 }
